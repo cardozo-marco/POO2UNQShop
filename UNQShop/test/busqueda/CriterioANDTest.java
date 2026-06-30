cb28c051f30b927c9b49f20d57f5dc6b806ad85a
Rocio Belen Mamani <rocidxs@gmail.com>
2026-06-30 17:38:29 -0300

    modifico CriterioANDTest y CriterioORTest

--- UNQShop/test/busqueda/CriterioANDTest.java
+++ UNQShop/test/busqueda/CriterioANDTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -36,4 +37,23 @@ class CriterioANDTest {
 
         assertFalse(sut.satisface(item), "AND debe fallar si uno es falso");
 	}
+	
+	@Test
+    void testNoSatisfaceSiAlMenosUnFiltroEsFalse() {
+        List<CriterioBusqueda> filtros = Arrays.asList(f1, f2);
+        CriterioAND sut = new CriterioAND(filtros);
+		
+        when(f1.satisface(item)).thenReturn(false);
+        when(f2.satisface(item)).thenReturn(true);
+
+        assertFalse(sut.satisface(item), "AND debe fallar si al menos uno es false");
+	}
+
+	@Test
+	void testANDVacioEsTrue() {
+    	CriterioAND sut = new CriterioAND(new ArrayList<>());
+    	assertTrue(sut.satisface(item), "Un AND sin filtros no restringe nada (true)");
+	}
+	
+
 }
--- UNQShop/test/busqueda/CriterioORTest.java
+++ UNQShop/test/busqueda/CriterioORTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -35,4 +36,23 @@ class CriterioORTest {
 
         assertTrue(sut.satisface(item), "OR debe fallar si todos los filtros son falso");
 	}
+	
+	@Test
+    void testNoSatisfaceSiTodosLosFiltrosSonFalse() {
+        List<CriterioBusqueda> filtros = Arrays.asList(f1, f2);
+        CriterioOR sut = new CriterioOR(filtros);
+		
+        when(f1.satisface(item)).thenReturn(false);
+        when(f2.satisface(item)).thenReturn(false);
+
+        assertFalse(sut.satisface(item), "OR debe devolver false si todos sus filtros fallan");
+    }
+	
+	@Test
+    void testSatisfaceSiLaListaDeFiltrosEstaVacia() {
+        // Caso de borde: una bssqueda sin criterios debería fallar.
+        CriterioOR sut = new CriterioOR(new ArrayList<>());
+
+        assertFalse(sut.satisface(item), "Un OR sin opciones no puede satisfacer ninguna (false)");
+    }
 }
